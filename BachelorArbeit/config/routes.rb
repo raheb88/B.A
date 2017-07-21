079364bafacf723afc6baccd1f854c2b0703f757
Seminar_Nov <habdiam50070@th-nuernberg.de>
2017-07-21 20:25:16 +0200

    Singup users form UI

--- BachelorArbeit/config/routes.rb
+++ BachelorArbeit/config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   #  get 'seminar' , to: 'welcome#seminar'
     resources :seminars
 
+    get 'signup', to: 'users#new'
+    #resources :users,except: [:new]
+     post 'users', to: 'users#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
