9954c1a7f6f03c51db7cebe78ba4b8fbf1b23f30
Seminar_Nov <habdiam50070@th-nuernberg.de>
2017-07-21 16:38:54 +0200

    Add secure password and attribute to users

--- BachelorArbeit/db/schema.rb
+++ BachelorArbeit/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170720131517) do
+ActiveRecord::Schema.define(version: 20170721143311) do
 
   create_table "seminars", force: :cascade do |t|
     t.string   "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170720131517) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
