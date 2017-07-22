4b0ee7dc5672f39cad2609e9dc9872427d8101bf
Seminar_Nov <habdiam50070@th-nuernberg.de>
2017-07-22 17:26:20 +0200

    Create show action and add gravatar image

--- BachelorArbeit/app/controllers/users_controller.rb
+++ BachelorArbeit/app/controllers/users_controller.rb
@@ -27,8 +27,11 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
- end
+  end
 
+ def show
+   @user = User.find(params[:id])
+ end
 
   private
   def user_params
